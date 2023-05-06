d512d11a329252921078b4b1b7bd34dc246cd6f7
Anivinder Reddy <anivinder.boreddy-intl>
2023-05-06 18:06:11 +0530

    deleteing cart items

--- models/user.js
+++ models/user.js
@@ -40,6 +40,14 @@ userSchema.methods.addToCart = function(product) {
   return this.save();
 }
 
+userSchema.methods.removeFromCart = function(productId) {
+  const updatedCartItems = this.cart.items.filter((item) => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save()
+}
+
 // const getDb = require("../util/database").getDb;
 // const mongodb = require("mongodb");
 // const ObjectId = mongodb.ObjectId;
@@ -51,50 +59,11 @@ userSchema.methods.addToCart = function(product) {
 //     this._id = id;
 //   }
 
-//   save() {
-//     const db = getDb();
-//     return db.collection("users").insertOne(this);
-//   }
-
 //   static findById(userId) {
 //     const db = getDb();
 //     return db.collection("users").findOne({ _id: new ObjectId(userId) });
 //   }
 
-
-//   getCart() {
-//     const db = getDb();
-//     const productIds = this.cart.items.map((i) => {
-//       return i.productId;
-//     });
-//     return db
-//       .collection("products")
-//       .find({ _id: { $in: productIds } })
-//       .toArray()
-//       .then((products) => {
-//         return products.map((p) => {
-//           return {
-//             ...p,
-//             quantity: this.cart.items.find((i) => {
-//               return i.productId.toString() === p._id.toString();
-//             }).quantity,
-//           };
-//         });
-//       });
-//   }
-
-//   deleteItemFromCart(productId) {
-//     const updatedCartItems = this.cart.items.filter((item) => {
-//       return item.productId.toString() !== productId.toString();
-//     });
-//     const db = getDb();
-//     return db
-//       .collection("users")
-//       .updateOne(
-//         { _id: new ObjectId(this._id) },
-//         { $set: { cart: { items: updatedCartItems } } }
-//       );
-//   }
 
 //   addOrder() {
 //     const db = getDb();
